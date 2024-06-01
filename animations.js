de2a6f98664d9f625be373482e11fc34f45fee02
Mark Holden <mtholden17@gmail.com>
2024-06-01 15:34:05 -0400

    Updated the home page to be more visually engaging.

--- animations.js
+++ animations.js
@@ -2,7 +2,24 @@ document.addEventListener("DOMContentLoaded", function() {
     const target = document.getElementById('animated-text');
     const text = target.innerText;
     const borderFrame = document.getElementById('border-frame');
+    const buttonsContainer = document.getElementById('button-container');
 
+    prepareAndAnimateText(target, text);
+
+    setTimeout(() => {
+        adjustAndShowBorder(borderFrame, target);
+        // Display buttons after the border is shown
+        showButtons(buttonsContainer);
+    }, 3000); // Delay to sync with text animation
+
+    window.addEventListener('resize', () => adjustBorderOnResize(borderFrame, target));
+    document.addEventListener('fullscreenchange', () => adjustBorder(borderFrame, target));
+    document.addEventListener('mozfullscreenchange', () => adjustBorder(borderFrame, target)); // Firefox
+    document.addEventListener('webkitfullscreenchange', () => adjustBorder(borderFrame, target)); // Chrome, Safari, Opera
+    document.addEventListener('msfullscreenchange', () => adjustBorder(borderFrame, target)); // IE / Edge
+});
+
+function prepareAndAnimateText(target, text) {
     target.innerText = ''; // Clear initial text
     const characters = Array.from(text).map(char => {
         const span = document.createElement('span');
@@ -21,7 +38,9 @@ document.addEventListener("DOMContentLoaded", function() {
             }, 600*3);
         }, 1000 + i * 100);
     });
+}
 
+function adjustAndShowBorder(borderFrame, target) {
     setTimeout(() => {
         const textRect = target.getBoundingClientRect();
         borderFrame.style.setProperty('--top', `${textRect.top}px`);
@@ -31,9 +50,10 @@ document.addEventListener("DOMContentLoaded", function() {
         
         borderFrame.style.display = 'block'; // Make border visible
         borderFrame.style.animation = 'shrinkBorder 2s ease forwards';
-    }, 3000); // Delay to sync with text animation
+    }, 10); // Delay to sync with text animation
+}
 
-    
+function adjustBorderOnResize(borderFrame, target) {
     window.addEventListener('resize', () => {
     
         const textRect = target.getBoundingClientRect();
@@ -45,4 +65,12 @@ document.addEventListener("DOMContentLoaded", function() {
         borderFrame.style.display = 'block'; // Make border visible
 
     });
-});
+}
+
+function showButtons(buttonsContainer) {
+    setTimeout(() => {
+        buttonsContainer.style.display = 'flex'; // Change display to flex
+        buttonsContainer.style.opacity = 1; // Ensure it's fully visible
+    }, 2000); // Short delay to ensure border animation visually completes
+}
+
