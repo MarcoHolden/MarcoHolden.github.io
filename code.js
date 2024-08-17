eba6a03427dfeac07f66aa2774dd19504e4002d0
Mark Holden <mtholden17@gmail.com>
2024-08-17 00:14:35 -0400

    Fixed spelling mistakes prevelant throughout the website.

--- code.js
+++ code.js
@@ -1,39 +1,3 @@
-// const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-// let interval = null;
-
-// const screens = document.querySelectorAll(".screen");
-
-// screens.forEach(screen => {
-//   const name = screen.querySelector(".name");
-
-//   screen.onmouseenter = event => {  
-//     let iteration = 0;
-
-//     clearInterval(interval);
-
-//     interval = setInterval(() => {
-//       name.innerText = name.innerText
-//         .split("")
-//         .map((letter, index) => {
-//           if (index < iteration) {
-//             return name.dataset.value[index];
-//           }
-
-//           return letters[Math.floor(Math.random() * 26)];
-//         })
-//         .join("");
-
-//       if (iteration >= name.dataset.value.length) { 
-//         clearInterval(interval);
-//       }
-
-//       iteration += 1 / 3;
-//     }, 30);
-//   };
-// });
-
-
 document.addEventListener("DOMContentLoaded", function() {
     const projects = [
         {
